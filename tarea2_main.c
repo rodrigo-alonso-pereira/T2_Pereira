c97f0f1fe9beda9f50e6265c8d722fc0899ee04f
Rodrigo Pereira <rodrigo.pereira@usach.cl>
2024-12-18 18:52:50 -0300

    implementacion de lectura parametros de entrada

---
+++ tarea2_main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "tarea2.h"
+
+int main (int argc, char *argv[]){
+    
+    if (argc != 4) {
+        fprintf(stderr, "Uso: %s <tiempo_simulacion> <intervalo_llegada> <probabilidad de embarque>\n", argv[0]);
+        return 1;
+    }
+
+    int tiempo_simulacion = atoi(argv[1]);
+    int tiempo_intervalo = atoi(argv[2]);
+    float probabilidad_embarque = atof(argv[3]);
+
+    printf("Tiempo simulacion: %d\n", tiempo_simulacion);
+    printf("Intervalo de llegada: %d\n", tiempo_intervalo);
+    printf("Probabilidad de embarque: %.2f\n", probabilidad_embarque);
+
+    return 0;
+}
