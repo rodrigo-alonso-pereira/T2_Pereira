a6b3f2ec776be2a16f459ba7e45382a757fa6aad
Rodrigo Pereira <rodrigo.pereira@usach.cl>
2024-12-18 19:15:01 -0300

    implementacion operaciones de cola

--- tarea2_implementacion.c
+++ tarea2_implementacion.c
@@ -3,3 +3,11 @@
 #include "tarea2.h"
 
 /*------------- operaciones COLA -------------*/
+
+cola* crea_cola(void);
+int es_cola_vacia(cola *c);
+void encolar(cola* c, int d);
+void desencolar(cola* c);
+carga* frente(cola* c);
+carga* final(cola* c);
+void imprime_cola(cola* c);
